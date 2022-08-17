6d2f0ab8b2b335fa16e0100cd1c5bed9ae080c00
kimnnanni <kimnnanni@gmail.com>
2022-08-17 22:47:13 +0900

    chapter1_1.3 DAO의 확장

--- chapter1/src/main/java/springbook/user/dao/NUserDao.java
+++ chapter1/src/main/java/springbook/user/dao/NUserDao.java
@@ -3,9 +3,11 @@ package springbook.user.dao;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class NUserDao extends UserDao {
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        //N사 DB Connection Source
-        return null;
-    }
+public class NUserDao
+//        extends UserDao
+{
+//    public Connection getConnection() throws ClassNotFoundException, SQLException {
+//        //N사 DB Connection Source
+//        return null;
+//    }
 }
