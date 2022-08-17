8e1c2127e8eff761f80d7e948af4872ddb492967
kimnnanni <kimnnanni@gmail.com>
2022-08-17 22:56:40 +0900

    chapter1_1.4 제어의 역전(Ioc)

--- chapter1/src/main/java/springbook/user/dao/UserDaoTest.java
+++ chapter1/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -4,11 +4,6 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        //ConnectionMaker connectionMaker = new DConnectionMaker();
-        ConnectionMaker connectionMaker = new NConnectionMaker();
-
-        UserDao dao = new UserDao(connectionMaker);
-        //1. UserDao create
-        //2. ConnectionMaker Type 제공 - 의존관계 설정
+        UserDao dao = new DaoFactory().userDao();
     }
 }
