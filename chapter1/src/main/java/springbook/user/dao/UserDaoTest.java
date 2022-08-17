d7c5f9ee5d5fdaecff77ffb076319d2785b00bb3
kimnnanni <kimnnanni@gmail.com>
2022-08-18 00:08:53 +0900

    chapter1_1.8 XML을 이용한 설정

--- chapter1/src/main/java/springbook/user/dao/UserDaoTest.java
+++ chapter1/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -2,12 +2,14 @@ package springbook.user.dao;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
     }
 }
