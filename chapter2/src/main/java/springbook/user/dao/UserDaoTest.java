de596dc5e374e3d53d5bf0a9cdc94add75fe2977
kimnnanni <kimnnanni@gmail.com>
2022-08-18 22:17:22 +0900

    chapter2_2.2 UserDaoTest 개선

--- chapter2/src/main/java/springbook/user/dao/UserDaoTest.java
+++ chapter2/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,32 +1,40 @@
 package springbook.user.dao;
 
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        
+        JUnitCore.main("springbook.user.dao.UserDaoTest");
+    }
+
+    @Test
+    public void addAndGet() throws ClassNotFoundException, SQLException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
         UserDao dao = context.getBean("userDao", UserDao.class);
-        
+
         User user = new User();
-        user.setId("user");
-        user.setName("백기선");
-        user.setPassword("married");
-        
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
+
         dao.add(user);
-        
-        System.out.println(user.getId() + " 등록 성공");
-        
+
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        
-        System.out.println(user2.getId() + " 조회 성공");
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
     }
 }
