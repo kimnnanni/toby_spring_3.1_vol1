5b5e53d009d4d96e9d66dbf04680bb110a1920a0
kimnnanni <kimnnanni@gmail.com>
2022-08-18 23:20:00 +0900

    chapter2_2.4 스프링 테스트 적용

--- chapter2/src/main/java/springbook/user/dao/UserDaoTest.java
+++ chapter2/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -3,30 +3,48 @@ package springbook.user.dao;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "/applicationContext.xml")
+//@DirtiesContext
+@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
-    private UserDao dao;
+//    @Autowired
+//    private ApplicationContext context;
+
+    UserDao dao;
     private User user1;
     private User user2;
     private User user3;
 
     @Before
     public void setUp() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        this.dao = context.getBean("userDao", UserDao.class);
+//        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        this.dao = this.context.getBean("userDao", UserDao.class);
+        dao = new UserDao();
+
+        DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/testdb", "spring", "book", true);
+        dao.setDataSource(dataSource);
 
         this.user1 = new User("gyumee", "박성철", "springno1");
         this.user2 = new User("leegw700", "이길원", "springno2");
