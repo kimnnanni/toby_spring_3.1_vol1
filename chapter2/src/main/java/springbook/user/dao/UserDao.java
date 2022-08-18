52ca171ce6b1edd190fb832b11e03135266ce14e
kimnnanni <kimnnanni@gmail.com>
2022-08-18 22:46:01 +0900

    chapter2_2.3 개발자를 위한 테스팅 프레임워크 JUnit

--- chapter2/src/main/java/springbook/user/dao/UserDao.java
+++ chapter2/src/main/java/springbook/user/dao/UserDao.java
@@ -1,11 +1,10 @@
 package springbook.user.dao;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserDao {
     private DataSource dataSource;
@@ -26,9 +25,51 @@ public class UserDao {
     public User get(String id) throws ClassNotFoundException, SQLException {
         Connection c = connectionMaker.makeConnection();
 
-        return new User();
+        PreparedStatement ps = c.prepareStatement("");
+        ResultSet rs = ps.executeQuery();
+
+        User user = null;
+        if(rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        if(user == null) throw new EmptyResultDataAccessException(1);
+
+        return user;
     }
 
     //public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+
+    public void deleteAll() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("delete from users");
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public int getCount() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+    }
 }
 
