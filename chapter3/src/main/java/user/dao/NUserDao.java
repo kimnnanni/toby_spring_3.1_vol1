80a74b8c499a09508b0dd6600a0a6d013edf3612
kimnnanni <kimnnanni@gmail.com>
2022-08-19 21:30:33 +0900

    chapter3_init

---
+++ chapter3/src/main/java/user/dao/NUserDao.java
@@ -0,0 +1,13 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class NUserDao
+//        extends UserDao
+{
+//    public Connection getConnection() throws ClassNotFoundException, SQLException {
+//        //N사 DB Connection Source
+//        return null;
+//    }
+}
