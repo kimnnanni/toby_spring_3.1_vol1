b0454815b8097881097e996155111c817a7c9d93
kimnnanni <kimnnanni@gmail.com>
2022-08-22 23:33:02 +0900

    chapter4_4.2_예외전환

--- chapter4/src/main/java/springbook.user/dao/UserDaoDeleteAll.java
+++ chapter4/src/main/java/springbook.user/dao/UserDaoDeleteAll.java
@@ -4,10 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class UserDaoDeleteAll extends UserDao {
-    @Override
-    protected PreparedStatement makeStatement(Connection c) throws SQLException {
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        return ps;
-    }
+public class UserDaoDeleteAll extends UserDaoJdbc {
+//    @Override
+//    protected PreparedStatement makeStatement(Connection c) throws SQLException {
+//        PreparedStatement ps = c.prepareStatement("delete from users");
+//        return ps;
+//    }
 }
