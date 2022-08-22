1c143fe6773aa9bd1f0f6fef15c9d9439559cd9f
kimnnanni <kimnnanni@gmail.com>
2022-08-22 22:30:09 +0900

    chapter4_init

---
+++ chapter4/src/main/java/springbook.user/dao/DaoFactory.java
@@ -0,0 +1,24 @@
+package springbook.user.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DaoFactory {
+    @Bean
+    public UserDao userDao() {
+        return new UserDao(connectionMaker());
+    }
+
+//    public AccountDao accountDao() {
+//        return new AccountDao(new DConnectionMaker()); --> return new AccountDao(connectionMaker());
+//    }
+//
+//    public MessageDao messageDao() {
+//        return new MessageDao(new DConnectionMaker()); --> return new MessageDao(connectionMaker());
+//    }
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
+}
