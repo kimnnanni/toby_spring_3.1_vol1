c9f1d61363e43a64e5b1461e434e6109e15a17fd
kimnnanni <kimnnanni@gmail.com>
2022-08-25 23:00:41 +0900

    chapter5_5.1 사용자 레벨 관리 기능 추가

---
+++ chapter5/src/main/java/springbook.user/dao/NUserDao.java
@@ -0,0 +1,10 @@
+package springbook.user.dao;
+
+public class NUserDao
+//        extends UserDao
+{
+//    public Connection getConnection() throws ClassNotFoundException, SQLException {
+//        //N사 DB Connection Source
+//        return null;
+//    }
+}
