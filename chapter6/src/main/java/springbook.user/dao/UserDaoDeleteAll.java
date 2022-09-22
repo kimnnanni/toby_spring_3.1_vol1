f2fe7aae8fe015f43902c70015acdb9f4bb59499
kimnnanni <64414516+kimnnanni@users.noreply.github.com>
2022-09-22 23:55:42 +0900

    Revert "chapter6_init"

--- chapter6/src/main/java/springbook.user/dao/UserDaoDeleteAll.java
+++
@@ -1,9 +0,0 @@
-package springbook.user.dao;
-
-public class UserDaoDeleteAll extends UserDaoJdbc {
-//    @Override
-//    protected PreparedStatement makeStatement(Connection c) throws SQLException {
-//        PreparedStatement ps = c.prepareStatement("delete from users");
-//        return ps;
-//    }
-}
